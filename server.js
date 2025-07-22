dce8882741e7add949f295712f34649e89b1335f
Usuário <usuario@MacBook-Pro-de-Usuario.local>
2025-07-21 23:09:42 -0300

    Use process.env.PORT for Vercel deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para parsear o corpo das requisições
 app.use(express.urlencoded({ extended: true }));
